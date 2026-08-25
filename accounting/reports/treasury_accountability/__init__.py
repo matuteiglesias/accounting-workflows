70b9080c9a3776bb4acdc2b2248a0f444effebc6
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-25 17:04:43 -0300

    feat: productionize treasury accountability report
    * feat: add treasury accountability report spec
    
    * feat: productionize treasury accountability HTML renderer
    
    * feat: expose treasury accountability renderer
    
    * feat: add treasury accountability report styling
    
    * test: lock treasury accountability report semantics

---
+++ accounting/reports/treasury_accountability/__init__.py
@@ -0,0 +1,5 @@
+"""Monthly treasury accountability report over the governed accountability mart."""
+
+from accounting.reports.treasury_accountability.render import render_report
+
+__all__ = ["render_report"]
