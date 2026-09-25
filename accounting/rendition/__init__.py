3c175677d6d2e13288e521af36cfde7875fc6118
Matias Iglesias <matuteiglesias@gmail.com>
2026-09-25 00:41:36 -0300

    fix: keep rendition CLI module lazy

--- accounting/rendition/__init__.py
+++ accounting/rendition/__init__.py
@@ -3,7 +3,6 @@
 from accounting.rendition.contract import RENDITION_CONTRACT_SCHEMA, RENDITION_SPEC_SCHEMA
 from accounting.rendition.scope import PropertyRegistry, PropertyScope
 from accounting.rendition.spec import RenditionPeriod, RenditionSpec, load_rendition_spec, parse_rendition_spec
-from accounting.rendition.validation import RenditionValidationResult, validate_rendition_context
 
 __all__ = [
     "RENDITION_CONTRACT_SCHEMA",
@@ -12,8 +11,6 @@ __all__ = [
     "PropertyScope",
     "RenditionPeriod",
     "RenditionSpec",
-    "RenditionValidationResult",
     "load_rendition_spec",
     "parse_rendition_spec",
-    "validate_rendition_context",
 ]
