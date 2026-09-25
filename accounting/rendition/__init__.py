f493e53271cac2d4f29eb530e9c03a36dc4a213b
Matias Iglesias <matuteiglesias@gmail.com>
2026-09-25 00:51:22 -0300

    fix: keep rendition compiler import lazy

--- accounting/rendition/__init__.py
+++ accounting/rendition/__init__.py
@@ -2,19 +2,16 @@
 
 from accounting.rendition.contract import RENDITION_CONTRACT_SCHEMA, RENDITION_SPEC_SCHEMA
 from accounting.rendition.scope import PropertyRegistry, PropertyScope
-from accounting.rendition.build import MANDATORY_ACTIVITY_CAVEAT, compile_rendition
 from accounting.rendition.spec import OpeningBasis, RenditionPeriod, RenditionSpec, load_rendition_spec, parse_rendition_spec
 
 __all__ = [
     "RENDITION_CONTRACT_SCHEMA",
     "RENDITION_SPEC_SCHEMA",
-    "MANDATORY_ACTIVITY_CAVEAT",
     "PropertyRegistry",
     "PropertyScope",
     "OpeningBasis",
     "RenditionPeriod",
     "RenditionSpec",
-    "compile_rendition",
     "load_rendition_spec",
     "parse_rendition_spec",
 ]
