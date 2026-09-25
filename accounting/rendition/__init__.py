52bea7487d5691946779b25f333f7e1120bc13a3
Matias Iglesias <matuteiglesias@gmail.com>
2026-09-25 00:51:11 -0300

    feat: expose rendition compiler entry points

--- accounting/rendition/__init__.py
+++ accounting/rendition/__init__.py
@@ -2,15 +2,19 @@
 
 from accounting.rendition.contract import RENDITION_CONTRACT_SCHEMA, RENDITION_SPEC_SCHEMA
 from accounting.rendition.scope import PropertyRegistry, PropertyScope
-from accounting.rendition.spec import RenditionPeriod, RenditionSpec, load_rendition_spec, parse_rendition_spec
+from accounting.rendition.build import MANDATORY_ACTIVITY_CAVEAT, compile_rendition
+from accounting.rendition.spec import OpeningBasis, RenditionPeriod, RenditionSpec, load_rendition_spec, parse_rendition_spec
 
 __all__ = [
     "RENDITION_CONTRACT_SCHEMA",
     "RENDITION_SPEC_SCHEMA",
+    "MANDATORY_ACTIVITY_CAVEAT",
     "PropertyRegistry",
     "PropertyScope",
+    "OpeningBasis",
     "RenditionPeriod",
     "RenditionSpec",
+    "compile_rendition",
     "load_rendition_spec",
     "parse_rendition_spec",
 ]
